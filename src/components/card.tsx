41312decc364c30eaaeb5824bfd707383889e912
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-04-14 20:56:58 +0530

    Add functionality to home page (dynamic)

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,20 +1,28 @@
-export const Card = () => {
+import { Link } from "react-router-dom";
+
+interface CardProps {
+  id: string | number;
+  image: string;
+  title: string;
+  summary: string;
+}
+
+export const Card = ({ id, image, title, summary }: CardProps) => {
   return (
-    <div className="flex flex-col w-80 bg-gray-100 rounded-xl">
+    <div className="flex flex-col w-80  bg-gray-100 rounded-xl hover:transition hover:scale-105 transition-all hover:bg-gray-200">
       <div>
-        <img src="/35593.jpg" className="w-80 h-64 rounded-t-xl" />
+        <img src={image} className="w-80 h-64 rounded-t-xl" />
+      </div>
+      <div className="p-4 flex flex-col">
+        <h1 className="text-2xl font-bold text-gray-900 min-h-24">{title}</h1>
+        <p className="text-gray-500 line-clamp-3 ">{summary}</p>
       </div>
       <div className="p-4">
-        <h1 className="text-2xl font-bold text-gray-900 ">Recipe name here </h1>
-        <p className="text-gray-500 line-clamp-3">
-          Recipe description here with is currently harcoded and is very lonk
-          which can cover al least 2 lines and line clamp is three lines
-        </p>
-      </div>
-      <div className="p-4">
-        <button className="font-bold flex items-center text-xl bg-[#A34343] text-white px-6 py-2 rounded-lg">
-          Go
-        </button>
+        <Link to={`/recipe/${id}`} className="cursor-pointer">
+          <button className="font-bold flex items-center text-xl bg-[#A34343] text-white px-6 py-2 rounded-lg">
+            Go
+          </button>
+        </Link>
       </div>
     </div>
   );
--- src/components/recipe-cards.tsx
+++ src/components/recipe-cards.tsx
@@ -1,14 +1,47 @@
+import { useEffect, useState } from "react";
 import { Card } from "./card";
+import axios from "axios";
+
+interface Recipe {
+  id: number;
+  image: string;
+  title: string;
+  summary: string;
+}
 
 export const RecipeCards = () => {
+  const [randomRecipes, setRandomRecipes] = useState<Recipe[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await axios.get(
+          `https://api.spoonacular.com/recipes/random?number=12&apiKey=024fabe782cd49cca228066b00d6f529`
+        );
+        const data = await res.data;
+        setRandomRecipes(data.recipes);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getData();
+  }, []);
+
+  console.log(randomRecipes);
+
   return (
     <div className="grid grid-cols-3 gap-x-10 max-w-7xl mx-auto mt-10 gap-y-5 mb-10">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {randomRecipes &&
+        randomRecipes.map((recipe) => (
+          <Card
+            key={recipe.id}
+            id={recipe.id}
+            image={recipe.image}
+            title={recipe.title}
+            summary={recipe.summary}
+          />
+        ))}
     </div>
   );
 };
